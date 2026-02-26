b1ad34a5f81cdd3ba2b7cd43d16d1cfb51367c47
Akhilesh <amuppayyanmath@gmail.com>
2026-02-26 21:37:13 +0530

    Initial commit: Adding my DSA solutions

---
+++ 1.two-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++){
+            int number = target - nums[i];
+            if(map.containsKey(number)){
+                
+                 return new int[] { i, map.get(number) };
+            } else {
+                map.put(nums[i], i);
+            }
+
+
+        }
+        return new int[]{};
+    }
+}

1d9b9f8071ed53932ce5ad127f16b490af358303
yellowcupcoffe <amuppayyanmath@gmail.com>
2026-04-16 12:03:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,20 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> numbers = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            numbers.add(nums[i]);
-        }
+       
 
         HashSet<Integer> ans = new HashSet<>();    
-        for(int i=0;i<nums.length;i++){
-            if(numbers.contains(nums[i]) && ans.contains(nums[i]) ){ 
-                ans.remove(nums[i]);
-            } else{
-             ans.add(nums[i]);     
-            }
-        }
-
-        return ans.iterator().next();
+for (int n : nums) {
+    if (ans.contains(n)) {
+        ans.remove(n); 
+    } else {
+        ans.add(n);    
+    }
+}
+return ans.iterator().next();
 
     }
 }
